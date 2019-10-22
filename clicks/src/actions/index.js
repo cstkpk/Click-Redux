02dda7ac7efb847a228a866ea6227a492d66219f
Cara Sunberg <cnsunberg@gmail.com>
2019-10-22 16:58:11 -0400

    Removed unnecessary default value from parameter in action creator

--- clicks/src/actions/index.js
+++ clicks/src/actions/index.js
@@ -1,6 +1,6 @@
 // Action creators here
 
-export const increaseClickCount = (clickCount=0) => {
+export const increaseClickCount = (clickCount) => {
     return {
         type: "INCREASE_CLICK_COUNT",
         payload: clickCount
