aa36f716e6f4c515b2851470af0ebc13aefffd02
Cara Sunberg <cnsunberg@gmail.com>
2019-11-04 11:37:26 -0500

    Added images and finished image exexport array

--- clicks/src/assets/images/index.js
+++ clicks/src/assets/images/index.js
@@ -2,6 +2,26 @@
 // Refer to each image as an array item inside of reducer (i.e. add 1 to this index each click)
 module.exports = [
     {
+        image: require("./zero.gif"),
+        alt: "zero"
+    },
+    {
+        image: require("./one.gif"),
+        alt: "one"
+    },
+    {
+        image: require("./two.gif"),
+        alt: "two"
+    },
+    {
+        image: require("./three.gif"),
+        alt: "three"
+    },
+    {
+        image: require("./four.gif"),
+        alt: "four"
+    },
+    {
         image: require("./five.gif"),
         alt: "five"
     }
